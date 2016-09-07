5fc0935c8a25829e2b06f6b4d071590d4d080cfb
silion <lslshadow@163.com>
2016-09-07 14:11:32 +0800

    Update Jackson

--- app/src/main/java/com/silion/androidproject/jackson/JacksonActivity.java
+++ app/src/main/java/com/silion/androidproject/jackson/JacksonActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.silion.androidproject.R;
@@ -13,7 +14,9 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class JacksonActivity extends AppCompatActivity {
 
@@ -37,27 +40,6 @@ public class JacksonActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        /**
-         * ObjectMapper是JSON操作的核心，Jackson的所有JSON操作都是在ObjectMapper中实现。
-         * ObjectMapper有多个JSON序列化的方法，可以把JSON字符串保存File、OutputStream等不同的介质中。
-         * writeValue(File arg0, Object arg1)把arg1转成json序列，并保存到arg0文件中。
-         * writeValue(OutputStream arg0, Object arg1)把arg1转成json序列，并保存到arg0输出流中。
-         * writeValueAsBytes(Object arg0)把arg0转成json序列，并把结果输出成字节数组。
-         * writeValueAsString(Object arg0)把arg0转成json序列，并把结果输出成字符串。
-         */
-        ObjectMapper mapper = new ObjectMapper();
-
-        //User类转JSON
-        String json = null;
-        try {
-            json = mapper.writeValueAsString(user1);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        // {"birthday":"2014年09月30日","name":"silion","mail":"silion@qq.com"}
-        System.out.println(json);
-
-        //Java集合转JSON
         User user2 = new User();
         user2.setName("silion");
         user2.setEmail("silion@qq.com");
@@ -72,44 +54,127 @@ public class JacksonActivity extends AppCompatActivity {
         List<User> users = new ArrayList<>();
         users.add(user1);
         users.add(user2);
-        String jsonlist = null;
+
+        // User类转JSON
+        String json = object2Json(user1);;
+        // User类转JSON : {"birthday":"2014年09月30日","name":"silion","mail":"silion@qq.com"}
+        System.out.println("User类转JSON : " + json);
+
+        // Map转JSON
+        Map<String, User> map = new HashMap<>();
+        map.put("silion", user1);
+        String jsonmap = object2Json(map);
+        // Map转JSON : {"silion":{"birthday":"2014年09月30日","name":"silion","mail":"silion@qq.com"}}
+        System.out.println("Map转JSON : " + jsonmap);
+
+        // List转JSON
+        String jsonlist = object2Json(users);
+        // List转JSON : [{"birthday":"2014年09月30日","name":"silion","mail":"silion@qq.com"},{"birthday":"2014年12月31日","name":"silion","mail":"silion@qq.com"}]
+        System.out.println("List转JSON : " + jsonlist);
+    }
+
+    /**
+     * 将一个object转换为json, 可以使一个java对象，也可以使集合
+     *
+     * @param object
+     * @return
+     */
+    private String object2Json(Object object) {
+        /**
+         * ObjectMapper是JSON操作的核心，Jackson的所有JSON操作都是在ObjectMapper中实现。
+         * ObjectMapper有多个JSON序列化的方法，可以把JSON字符串保存File、OutputStream等不同的介质中。
+         * writeValue(File arg0, Object arg1)把arg1转成json序列，并保存到arg0文件中。
+         * writeValue(OutputStream arg0, Object arg1)把arg1转成json序列，并保存到arg0输出流中。
+         * writeValueAsBytes(Object arg0)把arg0转成json序列，并把结果输出成字节数组。
+         * writeValueAsString(Object arg0)把arg0转成json序列，并把结果输出成字符串。
+         */
+        ObjectMapper mapper = new ObjectMapper();
+
+        String json = null;
         try {
-            jsonlist = mapper.writeValueAsString(users);
+            json = mapper.writeValueAsString(object);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
-        // [{"birthday":"2014年09月30日","name":"silion","mail":"silion@qq.com"},{"birthday":"2014年12月31日","name":"silion","mail":"silion@qq.com"}]
-        System.out.println(jsonlist);
+
+        return json;
     }
 
     public void json2Java(View view) {
         String json = "{\"age\":2,\"birthday\":1412092800000,\"mail\":\"silion@qq.com\",\"name\":\"silion\"}";
+        String jsonmap = "{\"silion\":{\"birthday\":\"2014年09月30日\",\"name\":\"silion\",\"mail\":\"silion@qq.com\"}}";
         String jsonlist = "[{\"age\":2,\"birthday\":1412092800000,\"mail\":\"silion@qq.com\",\"name\":\"silion\"},{\"age\":2,\"birthday\":1420041600000,\"mail\":\"silion@qq.com\",\"name\":\"silion\"}]";
 
         /**
          * ObjectMapper支持从byte[]、File、InputStream、字符串等数据的JSON反序列化。
          */
-        ObjectMapper mapper = new ObjectMapper();
+        
+        // JSON转Class
+        User user = json2Class(json, User.class);
+        // JSON转Class : User{name='silion', age=0, birthday=Wed Oct 01 00:00:00 GMT+08:00 2014, email='silion@qq.com'}
+        System.out.println("JSON转Class : " + user);
 
-        // JSON转User类
-        User user = null;
+        // JSON转Map
+        Map<String, User> map = json2Map(jsonmap);
+        // JSON转Map : {silion=User{name='silion', age=0, birthday=Tue Sep 30 08:00:00 GMT+08:00 2014, email='silion@qq.com'}}
+        System.out.println("JSON转Map : " + map);
+
+        // JSON转List
+        List<User> users = json2List(jsonlist);
+        // JSON转List : [User{name='silion', age=0, birthday=Wed Oct 01 00:00:00 GMT+08:00 2014, email='silion@qq.com'}, User{name='silion', age=0, birthday=Thu Jan 01 00:00:00 GMT+08:00 2015, email='silion@qq.com'}]
+        System.out.println("JSON转List : " + users);
+
+        // JSON转List2
+        List<User> users2 = json2List2(jsonlist);
+        // JSON转List2 : [User{name='silion', age=0, birthday=Wed Oct 01 00:00:00 GMT+08:00 2014, email='silion@qq.com'}, User{name='silion', age=0, birthday=Thu Jan 01 00:00:00 GMT+08:00 2015, email='silion@qq.com'}]
+        System.out.println("JSON转List2 : " + users2);
+
+    }
+
+    private List<User> json2List2(String jsonlist) {
+        ObjectMapper mapper = new ObjectMapper();
+        List<User> users = null;
         try {
-            user = mapper.readValue(json, User.class);
+            users = mapper.readValue(jsonlist, new TypeReference<List<User>>(){
+            });
         } catch (IOException e) {
             e.printStackTrace();
         }
-        // User{name='silion', age=0, birthday=Wed Oct 01 00:00:00 GMT+08:00 2014, email='silion@qq.com'}
-        System.out.println(user);
+        return users;
+    }
 
-        // JSON转集合
+    private List<User> json2List(String json) {
+        ObjectMapper mapper = new ObjectMapper();
         List<User> users = null;
         try {
             JavaType javaType = mapper.getTypeFactory().constructParametricType(List.class, User.class);
-            users = (List<User>) mapper.readValue(jsonlist, javaType);
+            users = mapper.readValue(json, javaType);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        // [User{name='silion', age=0, birthday=Wed Oct 01 00:00:00 GMT+08:00 2014, email='silion@qq.com'}, User{name='silion', age=0, birthday=Thu Jan 01 00:00:00 GMT+08:00 2015, email='silion@qq.com'}]
-        System.out.println(users);
+        return users;
+    }
+
+    private Map<String, User> json2Map(String json) {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, User> map = null;
+        try {
+            map = mapper.readValue(json, new TypeReference<Map<String, User>>(){
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
+
+    private <T> T json2Class(String json, Class<T> c) {
+        ObjectMapper mapper = new ObjectMapper();
+        T t = null;
+        try {
+            t = mapper.readValue(json, c);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return t;
     }
 }
