7b848da7b2a3b47c6778c33faf2fb2d75f15f432
silion <lslshadow@163.com>
2016-09-09 13:40:07 +0800

    Add getDiskCacheDir

--- slibrary/src/main/java/com/samsung/slibrary/utils/PackageUtils.java
+++ slibrary/src/main/java/com/samsung/slibrary/utils/PackageUtils.java
@@ -3,6 +3,9 @@ package com.samsung.slibrary.utils;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.os.Environment;
+
+import java.io.File;
 
 /**
  * Created by silion on 2016/9/1.
@@ -21,5 +24,15 @@ public class PackageUtils {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public static File getDiskCacheDir (Context context, String name) {
+        String cachePath;
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState()) || !Environment.isExternalStorageRemovable()) {
+            cachePath = context.getExternalCacheDir().getPath();
+        } else {
+            cachePath = context.getCacheDir().getPath();
+        }
+        return new File(cachePath + File.separator + name);
     }
 }
