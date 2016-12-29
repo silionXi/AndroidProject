350042a32609e71d5e0b73c48a719d6a6f952425
LiangShilong <LiangShilong>
2016-12-29 08:22:13 +0800

    Fixed new version build error and set up RecycleView

---
+++ app/src/main/java/com/silion/androidproject/recycleview/RecycleViewActivity.java
@@ -0,0 +1,15 @@
+package com.silion.androidproject.recycleview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.silion.androidproject.R;
+
+public class RecycleViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycle_view);
+    }
+}
