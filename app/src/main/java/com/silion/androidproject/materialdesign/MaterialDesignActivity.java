23c230acabe27cce9bf4f9db9feef26c45f4ad76
silion <lslshadow9@gmail.com>
2017-01-22 16:42:28 +0800

    Material Design - Navigation View

--- app/src/main/java/com/silion/androidproject/materialdesign/MaterialDesignActivity.java
+++ app/src/main/java/com/silion/androidproject/materialdesign/MaterialDesignActivity.java
@@ -1,6 +1,8 @@
 package com.silion.androidproject.materialdesign;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -23,11 +25,20 @@ public class MaterialDesignActivity extends BaseActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.naviView);
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeAsUpIndicator(android.R.drawable.ic_menu_more);
         }
+        navigationView.setCheckedItem(R.id.menuCall);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
     }
 
     @Override
