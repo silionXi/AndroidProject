7cdbab10f66f1cb936a7c8347f42203a175cc3d8
silion <lslshadow9@gmail.com>
2017-03-07 16:46:29 +0800

    LitePal增加表

---
+++ app/src/main/java/com/silion/androidproject/litepal/model/Comment.java
@@ -0,0 +1,26 @@
+package com.silion.androidproject.litepal.model;
+
+/**
+ * Created by silion on 2017/3/7.
+ */
+
+public class Comment {
+    private int id;
+    private String content;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
