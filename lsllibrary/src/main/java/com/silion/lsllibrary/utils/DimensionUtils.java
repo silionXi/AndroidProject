d9e6a299d4f41c132e2af48f940e0d1b9621a906
silion <lslshadow9@gmail.com>
2017-07-17 15:34:21 +0800

    dp转成px

--- lsllibrary/src/main/java/com/silion/lsllibrary/utils/DimensionUtils.java
+++ lsllibrary/src/main/java/com/silion/lsllibrary/utils/DimensionUtils.java
@@ -1,6 +1,7 @@
 package com.silion.lsllibrary.utils;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.util.TypedValue;
 
 /**
@@ -14,5 +15,15 @@ public class DimensionUtils {
 
     public static int sp2px(Context context, int sp) {
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, context.getResources().getDisplayMetrics());
+    }
+
+    /**
+     * dp转成px
+     *
+     * @param dp
+     * @return
+     */
+    public static int dp2px(float dp) {
+        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
     }
 }
