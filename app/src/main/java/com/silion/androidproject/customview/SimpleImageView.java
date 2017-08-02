f06c80d9aa9625a0704a32dcd79f156fa6e03f13
silion <lslshadow9@gmail.com>
2017-08-02 11:32:24 +0800

    Add Comment

--- app/src/main/java/com/silion/androidproject/customview/SimpleImageView.java
+++ app/src/main/java/com/silion/androidproject/customview/SimpleImageView.java
@@ -70,7 +70,7 @@ public class SimpleImageView extends View {
 
         // 保存画布状态
         canvas.save();
-        // 顺时针?旋转90°
+        // 顺时针?旋转90°{@link P44}
         canvas.rotate(90);
 
         //绘制文字
