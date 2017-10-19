d5d0be2aa70e13d9ba02b9e5292ec0ee54abd9bd
silion <lslshadow9@gmail.com>
2017-10-19 15:14:01 +0800

    Add Application

--- app/src/main/java/com/silion/androidproject/MyApplication.java
+++ app/src/main/java/com/silion/androidproject/MyApplication.java
@@ -1,7 +1,10 @@
 package com.silion.androidproject;
 
+import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
 
 import com.silion.androidproject.otto.MainThreadBus;
 import com.squareup.leakcanary.LeakCanary;
@@ -16,13 +19,72 @@ import org.litepal.LitePalApplication;
  */
 
 public class MyApplication extends LitePalApplication {
+    private static final String TAG = "MyApplication";
+    private static MyApplication mApplication = null;
     private static Context mContext;
     private static RefWatcher mRefWatcher;
     private static Bus mBus = new MainThreadBus();
 
+    private ActivityLifecycleCallbacks mActivityLifecycleCallbacks = new ActivityLifecycleCallbacks() {
+        @Override
+        public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+            Log.d(TAG, activity.getClass().getSimpleName() + " onCreated");
+        }
+
+        @Override
+        public void onActivityStarted(Activity activity) {
+
+        }
+
+        @Override
+        public void onActivityResumed(Activity activity) {
+
+        }
+
+        @Override
+        public void onActivityPaused(Activity activity) {
+
+        }
+
+        @Override
+        public void onActivityStopped(Activity activity) {
+
+        }
+
+        @Override
+        public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+        }
+
+        @Override
+        public void onActivityDestroyed(Activity activity) {
+            Log.d(TAG, activity.getClass().getSimpleName() + " onDestroyed");
+        }
+    };
+
+    /**
+     * silion_comment:
+     * 不应该在构造方法里初始化一些需要Context引用操作得到的数据，例如：
+     * getResources(), getPackageName, getSystemService()等
+     * 因为在{@link #attachBaseContext(Context)}}才传入Context给mBase
+     */
+    public MyApplication() {
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
+        /**
+         * silion_comment:
+         * 正确打开Application单例模式的姿势
+         * 1. 定义静态成员变量：private static MyApplication mApplication = null;
+         * 2. 在onCreate赋值：mApplication = this;
+         * 3. 静态方法提供实例：{@link #getApplication()}
+         */
+        mApplication = this;
+
+        registerActivityLifecycleCallbacks(mActivityLifecycleCallbacks);
+
         if (LeakCanary.isInAnalyzerProcess(this)) {
             // This process is dedicated to LeakCanary for heap analysis.
             // You should not init your app in this process.
@@ -32,6 +94,16 @@ public class MyApplication extends LitePalApplication {
         }
         ZXingLibrary.initDisplayOpinion(this);
         mContext = getApplicationContext();
+    }
+
+    @Override
+    public void onTerminate() {
+        unregisterActivityLifecycleCallbacks(mActivityLifecycleCallbacks);
+        super.onTerminate();
+    }
+
+    public static MyApplication getApplication() {
+        return mApplication;
     }
 
     public static Context getContext() {
